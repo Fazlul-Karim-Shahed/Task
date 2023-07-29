b1e654b3a5414b14917d496052ea6301ef44d878
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2023-07-30 01:34:28 +0600

    Bug fix

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ const deleteTaskStorage = number => {
 
 const createList = (val, index) => {
     let li = document.createElement('li')
-    li.classList.add('list-group-item', 'my-3', 'border-0', 'border-bottom', 'task_list_li')
+    li.classList.add('list-group-item', 'my-2', 'border-0', 'border-bottom', 'task_list_li')
     li.appendChild(document.createTextNode(val))
 
     let btn = document.createElement('button')
@@ -128,7 +128,7 @@ const taskSaveStorage = value => {
 
 const filterTask = (event) => {
     let val = event.target.value.toLowerCase()
-    console.log('val: ' +val);
+    console.log('val: ' + val);
     let li = document.querySelectorAll('.task_list_li')
     li.forEach((item, index) => {
         let i = item.firstChild.textContent.toLowerCase()
@@ -179,16 +179,16 @@ document.getElementById('complete_all').addEventListener('click', () => {
 document.getElementById('task_submit_btn').addEventListener('click', () => {
     let val = document.getElementById('task_input').value;
 
-    if(val === ''){
+    if (val === '') {
         alert('Please write something')
     }
-    else{
+    else {
         taskSaveStorage(val)
         taskGetStorage()
         document.getElementById('task_input').value = ''
     }
 
-    
+
 
 })
 
